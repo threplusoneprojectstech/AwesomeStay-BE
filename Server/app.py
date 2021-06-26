8049600c718b57abf218ac7278ff07c38d50f8b7
cornelius tantius <corneliustantius.ct@gmail.com>
2021-06-26 19:15:02 +0700

    add procfile

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from Server.Routes.MainRoute import MainRoute
 from Server.Routes.AuthRoute import AuthRoute
+import os
 
+os.system("pyclean .")
 app = FastAPI()
 
 app.include_router(MainRoute, tags=["Main"])
