102be0e130c5507a1b6ab04e3e0e284804edae8f
cornelius tantius <corneliustantius.ct@gmail.com>
2021-06-26 20:01:59 +0700

    update procfile

--- run_asgi.py
+++ run_asgi.py
@@ -3,5 +3,5 @@ import sys
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("Server.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("Server.app:app", host="0.0.0.0", port=5000, reload=True)
     os.system("pyclean .")
