ffa8f6335ab64a2360f28c7a31e1e11844e023ca
cornelius tantius <corneliustantius.ct@gmail.com>
2021-06-26 19:31:20 +0700

    update config load method

--- Server/Controller/DatabaseController.py
+++ Server/Controller/DatabaseController.py
@@ -1,8 +1,17 @@
+import os
 from json import load
 from pymongo import MongoClient
 
 def initialize_database(collection_name):
-    config = load( open("Server/config.json") )
-    connection_string = config["DBUri"]
+    connection_string = ""
+    try:
+        config = load( open("Server/config.json") )
+        connection_string = config["DBUri"]
+    except:
+        print("> loading config from os variable")
+        try:
+            connection_string = os.environ["DBUri"]
+        except:
+            print("> failed to load from environment variable")
     client = MongoClient(connection_string)["master"]
     return client[collection_name]
