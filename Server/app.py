40d13f3a4ae1e0ba702b476982e5895c4ace072c
cornelius tantius <corneliustantius.ct@gmail.com>
2021-06-26 21:20:40 +0700

    add cors policy

--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Server.Routes.MainRoute import MainRoute
 from Server.Routes.AuthRoute import AuthRoute
 import os
 
 os.system("pyclean .")
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(MainRoute, tags=["Main"])
 app.include_router(AuthRoute, tags=["Auth"])
